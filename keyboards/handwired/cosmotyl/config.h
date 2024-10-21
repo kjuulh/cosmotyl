544bd1ff95504ea79d7975a90ff91edbdb1d3cd7
kjuulh <contact@kjuulh.io>
2024-10-21 08:50:38 +0200

    feat: add keymap
    Signed-off-by: kjuulh <contact@kjuulh.io>

--- keyboards/handwired/cosmotyl/config.h
+++ keyboards/handwired/cosmotyl/config.h
@@ -25,3 +25,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP17
+
+#define TAPPING_TERM 150
